006f6f3ef3bb369f71e691318c79111cbb2f7e39
NazarStf <hworker98@gmail.com>
2023-07-27 23:27:56 +0300

    SpriteKit - The first steps

--- SpriteKit Animation/ContentView.swift
+++ SpriteKit Animation/ContentView.swift
@@ -6,21 +6,53 @@
 //
 
 import SwiftUI
+import SpriteKit
+
+class GameScene: SKScene {
+	override func didMove(to view: SKView) {
+		physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let touch = touches.first else { return }
+		let location = touch.location(in: self)
+		let ball = SKSpriteNode(imageNamed: "dodgeBall")
+		ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.7)
+		ball.physicsBody?.restitution = 0.4
+		ball.position = location
+		addChild(ball)
+	}
+	
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let touch = touches.first else { return }
+		let location = touch.location(in: self)
+		let ball = SKSpriteNode(imageNamed: "dodgeBall")
+		ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.7)
+		ball.physicsBody?.restitution = 0.4
+		ball.position = location
+		addChild(ball)
+	}
+}
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	var scene: SKScene {
+		let scene = GameScene()
+		scene.size = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+		return scene
+	}
+	
+	var body: some View {
+		ZStack {
+			VStack {
+				SpriteView(scene: scene)
+					.ignoresSafeArea()
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
