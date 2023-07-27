d116eba0c0aa236939e21c5527aeec2b0046ab72
NazarStf <hworker98@gmail.com>
2023-07-27 23:20:01 +0300

    Initial Commit

---
+++ SpriteKit Animation/SpriteKit_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKit_AnimationApp.swift
+//  SpriteKit Animation
+//
+//  Created by NazarStf on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKit_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
